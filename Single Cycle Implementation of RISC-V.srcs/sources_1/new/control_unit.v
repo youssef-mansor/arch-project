9d38e0e4a3969f6111edb2438fb2178048459ab0
Ahmed Allam <ahmed39652003@gmail.com>
2024-03-26 23:14:13 +0200

    Added support for I-type instructions in Control Unit

--- Single Cycle Implementation of RISC-V.srcs/sources_1/new/control_unit.v
+++ Single Cycle Implementation of RISC-V.srcs/sources_1/new/control_unit.v
@@ -20,55 +20,63 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module control_unit(
-    input [4:0] Inst_6_2, //represet instruction[6:2]
-    output reg Branch,
-    output reg MemRead,
-    output reg MemtoReg,
-    output reg [1:0] ALUOp,
-    output reg MemWrite,
-    output reg ALUsrc,
-    output reg RegWrite
-    );
-    
+module control_unit(input [4:0]      Inst_6_2, //represet instruction[6:2]
+                    output reg       Branch,
+                    output reg       MemRead,
+                    output reg       MemtoReg,
+                    output reg [1:0] ALUOp,
+                    output reg       MemWrite,
+                    output reg       ALUsrc,
+                    out put reg RegWrite
+                    );
+
    always @(*) begin
-        case(Inst_6_2)
-            5'b01100: begin
-                           Branch = 0;
-                           MemRead <= 0;
-                           MemtoReg <= 0;
-                           ALUOp = 2'b10;
-                           MemWrite <= 0;
-                           ALUsrc <= 0;
-                          RegWrite <= 1;
-                      end
-            5'b00000:begin
-                          Branch <= 0;
-                          MemRead <= 1;
-                          MemtoReg <= 1;
-                          ALUOp <= 2'b00;
-                          MemWrite <= 0;
-                          ALUsrc <= 1;
-                         RegWrite <= 1;
-                     end//
-            5'b01000: begin
-                          Branch <= 0;
-                          MemRead <= 0;
-                          MemtoReg <= 1'bx;//don't care
-                          ALUOp <= 2'b00;
-                          MemWrite <= 1;
-                          ALUsrc <= 1;
-                         RegWrite <= 0;
-                     end//
-            5'b11000: begin
-                          Branch <= 1;
-                          MemRead <= 0;
-                          MemtoReg <= 1'bx;//don't care
-                          ALUOp <= 2'b01;
-                          MemWrite <= 0;
-                          ALUsrc <= 0;
-                            RegWrite <= 0;
-                        end//
+      case(Inst_6_2)
+        5'b01100: begin
+           Branch = 0;
+           MemRead <= 0;
+           MemtoReg <= 0;
+           ALUOp = 2'b10;
+           MemWrite <= 0;
+           ALUsrc <= 0;
+           RegWrite <= 1;
+        end
+        5'b00000:begin
+           Branch <= 0;
+           MemRead <= 1;
+           MemtoReg <= 1;
+           ALUOp <= 2'b00;
+           MemWrite <= 0;
+           ALUsrc <= 1;
+           RegWrite <= 1;
+        end//
+        5'b01000: begin
+           Branch <= 0;
+           MemRead <= 0;
+           MemtoReg <= 1'bx;//don't care
+           ALUOp <= 2'b00;
+           MemWrite <= 1;
+           ALUsrc <= 1;
+           RegWrite <= 0;
+        end//
+        5'b00100: begin
+           Branch <= 0;
+           MemRead <= 0;
+           MemtoReg <= 0;
+           ALUOp <= 2'b10;
+           MemWrite <= 0;
+           ALUsrc <= 1;
+           RegWrite <= 1;
+        end//
+        5'b11000: begin
+           Branch <= 1;
+           MemRead <= 0;
+           MemtoReg <= 1'bx;//don't care
+           ALUOp <= 2'b01;
+           MemWrite <= 0;
+           ALUsrc <= 0;
+           RegWrite <= 0;
+        end//
             default: ;//don't do anything
         endcase
     end
