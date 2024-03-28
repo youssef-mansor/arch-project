cc75c410108b33903b03ade0684d9e74feb505bd
Ahmed Allam <ahmed39652003@gmail.com>
2024-03-28 06:31:25 +0200

    Rewrote ALU file

--- Single Cycle Implementation of RISC-V.srcs/sources_1/new/ALU_control_unit.v
+++ Single Cycle Implementation of RISC-V.srcs/sources_1/new/ALU_control_unit.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/27/2024 01:37:55 PM
-// Design Name: 
-// Module Name: ALU_control_unit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module ALU_control_unit(
     input [1:0] ALUOp,
