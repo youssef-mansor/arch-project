860761b3b36a81fbe1117726819cef69f5b95fdc
Ahmed Allam <ahmed39652003@gmail.com>
2024-03-28 05:35:05 +0200

    Added support for ecall and ebreak instructions

--- Single Cycle Implementation of RISC-V.srcs/sources_1/new/control_unit.v
+++ Single Cycle Implementation of RISC-V.srcs/sources_1/new/control_unit.v
@@ -21,13 +21,15 @@
 
 
 module control_unit(input [4:0]      Inst_6_2, //represet instruction[6:2]
+                    input Inst_20,
                     output reg [1:0] Branch,
                     output reg       MemRead,
                     output reg       MemtoReg,
                     output reg [3:0] ALUOp,
                     output reg       MemWrite,
                     output reg       ALUsrc,
-                    out put reg RegWrite
+                    output reg       RegWrite
+                    output reg       pc_halt
                     );
 
    always @(*) begin
@@ -40,6 +42,7 @@ module control_unit(input [4:0]      Inst_6_2, //represet instruction[6:2]
            MemWrite <= 0;
            ALUsrc <= 0;
            RegWrite <= 1;
+           pc_halt <= 0;
         end
         5'b00000:begin
            Branch <= 2'b00;
@@ -49,6 +52,7 @@ module control_unit(input [4:0]      Inst_6_2, //represet instruction[6:2]
            MemWrite <= 0;
            ALUsrc <= 1;
            RegWrite <= 1;
+           pc_halt <= 0;
         end//
         5'b01000: begin
            Branch <= 2'b00;
@@ -58,6 +62,7 @@ module control_unit(input [4:0]      Inst_6_2, //represet instruction[6:2]
            MemWrite <= 1;
            ALUsrc <= 1;
            RegWrite <= 0;
+           pc_halt <= 0;
         end//
         5'b00100: begin
            Branch <= 2'b00;
@@ -67,6 +72,7 @@ module control_unit(input [4:0]      Inst_6_2, //represet instruction[6:2]
            MemWrite <= 0;
            ALUsrc <= 1;
            RegWrite <= 1;
+           pc_halt <= 0;
         end//
         5'b11000: begin
            Branch <= 2'b01;
@@ -76,6 +82,7 @@ module control_unit(input [4:0]      Inst_6_2, //represet instruction[6:2]
            MemWrite <= 0;
            ALUsrc <= 0;
            RegWrite <= 0;
+           pc_halt <= 0;
         end//
         5'b11011: begin //JAL
            Branch <= 2'b10;
@@ -85,6 +92,7 @@ module control_unit(input [4:0]      Inst_6_2, //represet instruction[6:2]
            MemWrite <= 0;
            ALUsrc <= 1;
            RegWrite <= 1;
+           pc_halt <= 0;
          end//
         5'b11001: begin//JALR
            Branch <= 2'b11;
@@ -94,7 +102,8 @@ module control_unit(input [4:0]      Inst_6_2, //represet instruction[6:2]
            MemWrite <= 0;
            ALUsrc <= 1;
            RegWrite <= 1;
-           default: ;//don't do anything
+           pc_halt <= 0;
+         end//
          5'b00011: begin // fence
             Branch <= 2'b00;
             MemRead <= 0;
@@ -103,6 +112,25 @@ module control_unit(input [4:0]      Inst_6_2, //represet instruction[6:2]
             MemWrite <= 0;
             ALUsrc <= 0;
             RegWrite <= 0;
+            pc_halt <= 0;
+         end
+         5'b11100: begin // ecall or ebreak
+            Branch <= 2'b00;
+            MemRead <= 0;
+            MemtoReg <= 1'bx;//don't care
+            ALUOp <= 4'b0000;
+            MemWrite <= 0;
+            ALUsrc <= 0;
+            RegWrite <= 0;
+
+            if (Inst_20 == 1) begin
+               pc_halt <= 0;
+            end
+
+            else begin
+               pc_halt <= 1;
+            end
+         end
         endcase
     end
     
