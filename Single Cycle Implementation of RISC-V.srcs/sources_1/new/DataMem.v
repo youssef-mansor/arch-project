fedb269b6b7052b9ef944bb158323151ad983edf
youssef-mansor <youssef3453@aucegypt.edu>
2024-04-13 18:33:58 +0200

    add storing instructions SW, SH, SB

--- Single Cycle Implementation of RISC-V.srcs/sources_1/new/DataMem.v
+++ Single Cycle Implementation of RISC-V.srcs/sources_1/new/DataMem.v
@@ -19,12 +19,13 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+`include "defines.v" 
 
 module DataMem(
     input clk, 
     input MemRead, 
     input MemWrite,
-    input [7:0] addr, //address of a byte
+    input [7:0] addr, //address of byte
     input [31:0] data_in,
     input [2:0] funct3, 
     output reg [31:0] data_out);
@@ -39,11 +40,11 @@ module DataMem(
 //          {mem[3], mem[2], mem[1], mem[0]} = 32'd17;
 //          {mem[7], mem[6], mem[5], mem[4]} = 32'd9;
 //          {mem[11], mem[10], mem[9], mem[8]} = 32'd15;
-          {mem[3], mem[2], mem[1], mem[0]} = 32'h12345678;
-          {mem[7], mem[6], mem[5], mem[4]} = 32'h87654321;
-          {mem[11], mem[10], mem[9], mem[8]} = 32'hFFFFFFFF;
-          {mem[15], mem[14], mem[13], mem[12]} = 32'h0000007F;
-          {mem[19], mem[18], mem[17], mem[16]} = 32'h00007FFF;
+//          {mem[3], mem[2], mem[1], mem[0]} = 32'h12345678;
+//          {mem[7], mem[6], mem[5], mem[4]} = 32'h87654321;
+//          {mem[11], mem[10], mem[9], mem[8]} = 32'hFFFFFFFF;
+//          {mem[15], mem[14], mem[13], mem[12]} = 32'h0000007F;
+//          {mem[19], mem[18], mem[17], mem[16]} = 32'h00007FFF;
 
 //        mem[0] = 32'd3;
 //        mem[1] = 32'd4;
@@ -54,7 +55,21 @@ module DataMem(
     always @(posedge clk) begin
         if (MemWrite) begin
             // Write data to memory at the specified address
-            mem[addr] <= data_in;
+            case(funct3) 
+              `F3_SB: mem[addr] = data_in[7:0];
+              `F3_SH: 
+                  begin 
+                    mem[addr] = data_in[7:0];
+                    mem[addr+1] = data_in[15:8];
+                  end
+              `F3_SW:
+                   begin 
+                    mem[addr] = data_in[7:0];
+                    mem[addr+1] = data_in[15:8];
+                    mem[addr+2] = data_in[23:15];
+                    mem[addr+3] = data_in[31:24];
+                   end
+            endcase
         end
     end
     
@@ -65,11 +80,11 @@ module DataMem(
             case(funct3)
 //              3'b011://ld; 
 //              3'b110://lwu;
-                3'b000: data_out <= {{24{mem[addr][7]}},mem[addr]};                 //lb
-                3'b001: data_out <= {{16{mem[addr+1][7]}},mem[addr+1],mem[addr]};   //lh       Not working 
-                3'b010: data_out <= {mem[addr+3],mem[addr+2],mem[addr+1],mem[addr]};//lw         
-                3'b100: data_out <= {24'd0,mem[addr]};                              //lbu
-                3'b101: data_out <= {16'd0,mem[addr+1],mem[addr]};                //lhu
+                `F3_LB: data_out <= {{24{mem[addr][7]}},mem[addr]};                 //lb
+                `F3_LH: data_out <= {{16{mem[addr+1][7]}},mem[addr+1],mem[addr]};   //lh       Not working - visited, it seems to work just fin 4-10-2024
+                `F3_LW: data_out <= {mem[addr+3],mem[addr+2],mem[addr+1],mem[addr]};//lw         
+                `F3_LBU: data_out <= {24'd0,mem[addr]};                              //lbu
+                `F3_LHU: data_out <= {16'd0,mem[addr+1],mem[addr]};                //lhu
                 default: data_out <= 0;
             endcase 
         end
